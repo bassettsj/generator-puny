f8c16a4f81124219e5586aae246548d3da407487
Steven Bassett <steven.j.bassett@gmail.com>
2015-04-29 20:15:59 -0700

    initial commit

---
+++ app/index.js
@@ -0,0 +1,60 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the delightful ' + chalk.red('Puny') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'appName',
+      message: 'What would you like to call this project',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+      this.fs.copy(
+        this.templatePath('gitattrs')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
